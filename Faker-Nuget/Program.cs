a55c9f1a56a8b5a6c0a1db12afb6747db75e1772
Rizvan Gataev <Rizvan8103@gmail.com>
2022-03-02 10:52:15 +0200

    Faker-Nuget task
    Add Faker-Nuget task

---
+++ Faker-Nuget/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker_Nuget
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i < 3; i++)
+            {
+                PrintPerson();
+            }
+        }
+
+        private static void PrintPerson()
+        {
+            var firstName = Faker.Name.First();
+            var lastName = Faker.Name.Last();
+            var city = Faker.Address.City();
+            Console.WriteLine(firstName + " " + lastName + " " + city);
+        }
+    }
+}
